76441bfaf9bab06394e819a23aec2b25e406940f
InternetProvied <mathyscardoso28@gmail.com>
2026-06-06 09:55:01 +0200

    Update User.js

--- User.js
+++ User.js
@@ -1,38 +1,7 @@
 const mongoose = require('mongoose');
-
 const userSchema = new mongoose.Schema({
-    username: { 
-        type: String, 
-        required: true 
-    },
-    email: { 
-        type: String, 
-        default: 'Non renseigné' 
-    },
-    password: { 
-        type: String, 
-        default: 'oauth_account_secured' 
-    },
-    discordId: { 
-        type: String, 
-        default: 'N/A' 
-    },
-    ipAddress: { 
-        type: String, 
-        default: '127.0.0.1' 
-    },
-    avatar: { 
-        type: String, 
-        default: '' 
-    },
-    status: { 
-        type: String, 
-        default: 'Actif' 
-    },
-    role: { 
-        type: String, 
-        default: 'user' 
-    }
-}, { timestamps: true });
-
+    username: String,
+    email: { type: String, default: 'Non renseigné' },
+    role: { type: String, default: 'user' }
+});
 module.exports = mongoose.model('User', userSchema);
