379224a266c43bb9e3046f833421627ff922dd48
InternetProvied <mathyscardoso28@gmail.com>
2026-06-06 09:53:28 +0200

    Update User.js

--- User.js
+++ User.js
@@ -1,11 +1,38 @@
-<!DOCTYPE html> 
-<html lang="fr">
-<head><meta charset="UTF-8"><title>Login</title></head>
-<body>
-    <h1>Connexion</h1>
-    <form action="/login" method="POST">
-        <input name="username" placeholder="Pseudo" required>
-        <button type="submit">Connexion</button>
-    </form>
-</body>
-</html>
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: { 
+        type: String, 
+        required: true 
+    },
+    email: { 
+        type: String, 
+        default: 'Non renseigné' 
+    },
+    password: { 
+        type: String, 
+        default: 'oauth_account_secured' 
+    },
+    discordId: { 
+        type: String, 
+        default: 'N/A' 
+    },
+    ipAddress: { 
+        type: String, 
+        default: '127.0.0.1' 
+    },
+    avatar: { 
+        type: String, 
+        default: '' 
+    },
+    status: { 
+        type: String, 
+        default: 'Actif' 
+    },
+    role: { 
+        type: String, 
+        default: 'user' 
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model('User', userSchema);
